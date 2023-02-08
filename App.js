3e879cd7e720abbd90a4c3d3fe0b39ab9c776e27
Demchenk0 <santadyr2016@gmail.com>
2023-02-08 21:17:07 +0200

    task1

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import {
+	ImageBackground,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				style={styles.image}
+				source={require('./assets/image/PhotoBG.jpg')}
+			>
+				<Text>Регистрация</Text>
+				<TextInput style={styles.input} />
+			</ImageBackground>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
+	image: {
+		flex: 1,
+		resizeMode: 'cover',
+		justifyContent: 'center',
+		// alignItems: 'center',
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: '#f6f6f6',
+		marginHorizontal: 16,
+		height: 50,
+		borderRadius: 8,
+		color: '#212121',
+	},
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+https://monosnap.com/file/yrlnFcKhiQ46h4HnrjGEnrLgj0R3FY
